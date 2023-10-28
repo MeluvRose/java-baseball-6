0d238a84656652dc7659b499c9e10d4f46138f54
meluvrose <tanggu3910@gmail.com>
2023-10-28 21:02:52 +0900

    add: Make this test more specific

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,11 +1,24 @@
 package baseball;
 
 public class Validation {
+    public boolean isDigitStr(String val){
+        for (int i = 0; i < val.length(); i++){
+            char c = val.charAt(i);
+            if (c < '1' || c > '9') return false;
+        }
+        return true;
+    }
+    public boolean isValidLength(String val){
+        boolean valid = val.length() != 3;
+
+        return !valid;
+    }
 
     public boolean isValidate(String val, String option){
         if (option.equals("playing")){
-            boolean lineValid = val.length() != 3;
-            return !lineValid;
+            if (!isValidLength(val) || !isDigitStr(val)){
+                return false;
+            }
         }
         if (option.equals("over")){
             boolean choiceValid = (!val.equals("1")
